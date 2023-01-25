504521e2513fe726eaa70318a3dc2a3241c4c272
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2023-01-25 09:39:01 +0100

    initial commit

---
+++ src/test/java/io/sewah/jacocomavenplugindemo/JacocoMavenPluginDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sewah.jacocomavenplugindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacocoMavenPluginDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
